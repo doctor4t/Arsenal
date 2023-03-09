98e3a85953f8fc641de2ef11ae53394271a03d38
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-03-09 23:54:34 +0000

    pain

--- src/main/java/doctor4t/arsenal/mixin/client/InventoryScreenMixin.java
+++ src/main/java/doctor4t/arsenal/mixin/client/InventoryScreenMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
@@ -21,11 +22,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class InventoryScreenMixin extends AbstractInventoryScreen<PlayerScreenHandler> {
 	public InventoryScreenMixin(PlayerScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
 		super(screenHandler, playerInventory, text);
-		this.inventory = playerInventory;
 	}
 
-	@Unique private static final Identifier SLOT_TEXTURE = Arsenal.id("textures/gui/weapon_slot.png");
-	@Unique private final PlayerInventory inventory;
+//	@Unique private static final Identifier SLOT_TEXTURE = Arsenal.id("textures/item/weapon_slot.png");
+//	@Unique private WeaponSlotHolder inventory;
+
+//	@Inject(method = "<init>", at = @At(value = "TAIL"))
+//	private void arsenal$init(PlayerEntity player, CallbackInfo ci) {
+//		if (player.getInventory() instanceof WeaponSlotHolder holder) {
+//			this.inventory = holder;
+//		}
+//	}
 
 	@Inject(method = "drawBackground", at = @At(value = "TAIL"))
 	private void arsenal$drawSlots(MatrixStack matrices, float delta, int mouseX, int mouseY, CallbackInfo ci) {
@@ -35,9 +42,11 @@ public abstract class InventoryScreenMixin extends AbstractInventoryScreen<Playe
 		int i = this.x + 76;
 		int j = this.y + 43;
 		this.drawTexture(matrices, i, j, 76, 61, 18, 18);
-		if (this.inventory instanceof WeaponSlotHolder holder && holder.arsenal$getWeapon().isEmpty()) {
-			RenderSystem.setShaderTexture(0, SLOT_TEXTURE);
-			this.drawTexture(matrices, i + 1, j + 1, 0, 0, 16, 16);
-		}
+//		if (this.inventory.arsenal$getWeapon().isEmpty()) {					fixme: I don't know why, I don't want to know why, the texture won't render in the slot
+//			RenderSystem.setShader(GameRenderer::getPositionTexShader);
+//			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+//			RenderSystem.setShaderTexture(0, SLOT_TEXTURE);
+//			this.drawTexture(matrices, i + 1, j + 1, 0, 0, 16, 16);
+//		}
 	}
 }
